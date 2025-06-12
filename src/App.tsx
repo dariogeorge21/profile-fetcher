33ee28db40e697e8c58687929393c088c78851c2
Dario George <edu.dariogeorge21@gmail.com>
2025-06-12 12:06:17 +0530

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/footer'
 import Heading from './components/heading'
 import Input from './components/input'
 
@@ -10,6 +11,7 @@ function App() {
         <Heading />
       </div>
       <Input />
+      <Footer />
     </>
   )
 }
