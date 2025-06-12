b19df94a24017c149f1fb09eaf4e602150e7d095
Dario George <edu.dariogeorge21@gmail.com>
2025-06-12 12:02:41 +0530

    footer.tsx

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p className='footer-text'>Made with ❤️ by <a href="https://github.com/dariogeorge21">Dario George</a></p>
+      <p className='footer-text'>© 2025 Dario George</p>
+    </div>
+  )
+}
+
+export default Footer
